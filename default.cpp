5b35ae1582e047e5b7e1b1b86441316092362fd1
t1016d <t1016d@gmail.com>
2015-08-16 18:07:56 +0800

    add default code

---
+++ default.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define SZ(x) ((int)(x).size())
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+
+int main()
+{
+    
+    return 0;
+}
