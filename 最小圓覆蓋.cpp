5be69f4c3d3a39deee33ae5ee31e99d405f7c281
ppetter1025 <peter58972@gmail.com>
2015-09-26 17:07:28 +0800

    Update and rename 最小圓覆蓋 to 最小圓覆蓋.cpp

---
+++ 最小圓覆蓋.cpp
@@ -0,0 +1,106 @@
+#include <cstdlib>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+
+//#define test
+
+using namespace std;
+
+const int N = 1000000 + 10;
+
+struct PT {
+	double x, y;
+
+	PT() {}
+	PT(double x, double y):
+		x(x), y(y) {}
+	PT operator+(const PT &b) const {
+		return (PT) {x+b.x, y+b.y};
+	}
+	PT operator-(const PT &b) const {
+		return (PT) {x-b.x, y-b.y};
+	}
+	PT operator*(const double b) const {
+		return (PT) {x*b, y*b};
+	}
+	PT operator/(const double b) const {
+		return (PT) {x/b, y/b};
+	}
+	double operator^(const PT &b) const {
+		return x*b.y - y*b.x;
+	}
+
+	double len() const {
+		return sqrt(x*x + y*y);
+	}
+	PT T() const {
+		return (PT) {-y, x};
+	}
+} p[N];
+
+void update(PT a, PT b, PT c, PT &o, double &r) {
+	if(c.x < 0.0)	o = (a+b) / 2.0;
+	else {
+		PT p1 = (a+b)/2.0, p2 = p1 + (b-a).T();
+		PT p3 = (a+c)/2.0, p4 = p3 + (c-a).T();
+		double a123 = (p2-p1)^(p3-p1), a124 = (p2-p1)^(p4-p1);
+		if(a123 * a124 > 0.0)	a123 = -a123;
+		else	a123 = abs(a123), a124 = abs(a124);
+		o = (p4*a123 + p3*a124) / (a123 + a124);
+	}
+	r = (a-o).len();
+}
+
+int main() {
+    //freopen("C:/Users/S11/Desktop/pb.in", "r", stdin);
+
+	srand(7122);
+
+	int m, n;
+	while(scanf("%d%d", &m, &n)) {
+		if(!n && !m)	return 0;
+
+		for(int i = 0; i < n; i++)	scanf("%lf%lf", &p[i].x, &p[i].y);
+
+		for(int i = 0; i < n; i++)
+			swap(p[i], p[rand() % (i+1)]);
+
+		PT a = p[0], b = p[1], c(-1.0, -1.0), o = (a+b) / 2.0;
+		double r = (a-o).len();
+		for(int i = 2; i < n; i++) {
+			if((p[i]-o).len() <= r)	continue;
+
+			a = p[i];
+			b = p[0];
+			c = (PT) {-1.0, -1.0};
+			update(a, b, c, o, r);
+			for(int j = 1; j < i; j++) {
+				if((p[j]-o).len() <= r)	continue;
+
+				b = p[j];
+				c = (PT) {-1.0, -1.0};
+                update(a, b, c, o, r);
+
+                for(int k = 0; k < j; k++) {
+					if((p[k]-o).len() <= r)	continue;
+
+					c = p[k];
+					update(a, b, c, o, r);
+                }
+			}
+
+			#ifdef test
+			printf("i=%d\n", i);
+			printf("a=(%.1f, %.1f)\n", a.x, a.y);
+			printf("b=(%.1f, %.1f)\n", b.x, b.y);
+			printf("c=(%.1f, %.1f)\n", c.x, c.y);
+			printf("o=(%.1f, %.1f)\n", o.x, o.y);
+			printf("r=%.1f\n", r);
+			puts("-----");
+			#endif // test
+		}
+
+		printf("%.3f\n", r);
+	}
+}
