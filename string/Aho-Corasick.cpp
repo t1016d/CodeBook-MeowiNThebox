8778da7f1227e65d20261010174dadec77ff9410
TingWei Lin <t1016d@gmail.com>
2016-04-03 20:21:03 +0800

    fix Aho-Corasick

--- string/Aho-Corasick.cpp
+++ string/Aho-Corasick.cpp
@@ -1,9 +1,11 @@
+// AC code of UVa 10679
 #include <cstdio>
 #include <cstring>
 #include <new>
 
-struct Trie{
+struct Trie {
 	int c;
+	bool fi=0;
 	Trie *fail,*ch[52];
 	Trie():c(0){memset(ch,0,sizeof(ch));}
 }trie[1000100];
@@ -11,16 +13,13 @@ struct Trie{
 char m[1010],f[100100];
 Trie *str[1010],*na,*root;
 
-inline int c_i(char a)
-{
+inline int c_i(char a) {
 	return (a>='A' && a<='Z') ? a-'A' : a-'a'+26;
 }
 
-void insert(char *s,int num)
-{
+void insert(char *s,int num) {
 	Trie *at=root;
-	while(*s)
-	{
+	while(*s) {
 		if(!at->ch[c_i(*s)])
 			at->ch[c_i(*s)]=new (na++) Trie();
 		at=at->ch[c_i(*s)],s++;
@@ -31,16 +30,13 @@ void insert(char *s,int num)
 Trie *q[1000100];
 int ql,qr;
 
-void init()
-{
+void init() {
 	ql=qr=-1;
 	q[++qr]=root;
 	root->fail=NULL;
-	while(ql<qr)
-	{
+	while(ql<qr) {
 		Trie *n=q[++ql],*f;
-		for(int i=0;i<52;i++)
-		{
+		for(int i=0;i<52;i++) {
 			if(!n->ch[i])
 				continue;
 			f=n->fail;
@@ -52,11 +48,9 @@ void init()
 	}
 }
 
-void go(char *s)
-{
+void go(char *s) {
 	Trie*p=root;
-	while(*s)
-	{
+	while(*s) {
 		while(p && !p->ch[c_i(*s)])
 			p=p->fail;
 		p=p?p->ch[c_i(*s)]:root;
@@ -65,24 +59,20 @@ void go(char *s)
 	}
 }
 
-void AC()
-{
+void AC() {
 	for(int i=qr;i>0;i--)
 		q[i]->fail->c+=q[i]->c;
 }
 
-int main()
-{
+int main() {
 	int T,q;
 	scanf("%d",&T);
-	while(T--)
-	{
+	while(T--) {
 		na=trie;
 		root=new (na++) Trie();
 		scanf("%s",f);
 		scanf("%d",&q);
-		for(int i=0;i<q;i++)
-		{
+		for(int i=0;i<q;i++) {
 			scanf("%s",m);
 			insert(m,i);
 		}
