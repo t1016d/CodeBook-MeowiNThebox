f24b7f19f293e3a6f418a28d54eb8058e861ee20
TingWei Lin <t1016d@gmail.com>
2017-01-13 23:15:27 +0800

    Fix MillerRabin_other to unsigned long long

--- math/MillerRabin_other.cpp
+++ math/MillerRabin_other.cpp
@@ -1,23 +1,8 @@
-/* Miller Rabin code from ioicamp */
-#include <bits/stdc++.h>
-#define PB push_back
-#define MP make_pair
-#define F first
-#define S second
-#define SZ(x) ((int)(x).size())
-#define ALL(x) (x).begin(),(x).end()
-#ifdef _DEBUG_
-	#define debug(...) printf(__VA_ARGS__)
-#else
-	#define debug(...) 0
-#endif
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> PII;
-typedef vector<int> VI;
+//input should < 2^63 - 1 (max prime:9223372036854775783)
+typedef unsigned long long ull;
 
-ll mul(ll a, ll b, ll n) {
-	ll r = 0;
+ull mul(ull a, ull b, ull n) {
+	ull r = 0;
 	a %= n, b %= n;
 	while(b) {
 		if(b&1) r = (a+r>=n ? a+r-n : a+r);
@@ -27,17 +12,17 @@ ll mul(ll a, ll b, ll n) {
 	return r;
 }
 
-ll bigmod(ll a, ll d, ll n) {
+ull bigmod(ull a, ull d, ull n) {
 	if(d==0)	return 1LL;
 	if(d==1) return a % n;
 	return mul(bigmod(mul(a, a, n), d/2, n), d%2?a:1, n);
 }
 
 const bool PRIME = 1, COMPOSITE = 0;
-bool miller_rabin(ll n, ll a) {
+bool miller_rabin(ull n, ull a) {
 	if(__gcd(a, n) == n)	return PRIME;
 	if(__gcd(a, n) != 1)	return COMPOSITE;
-	ll d = n-1, r = 0, res;
+	ull d = n-1, r = 0, res;
 	while(d%2==0) { ++r; d/=2; }
 	res = bigmod(a, d, n);
 	if(res == 1 || res == n-1)	return PRIME;
@@ -48,10 +33,10 @@ bool miller_rabin(ll n, ll a) {
 	return COMPOSITE;
 }
 
-bool isprime(ll n) {
+bool isprime(ull n) {
 	if(n==1)
 		return COMPOSITE;
-	ll as[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+	ull as[7] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
 	for(int i=0; i<7; i++)
 		if(miller_rabin(n, as[i]) == COMPOSITE)	return COMPOSITE;
 	return PRIME;
