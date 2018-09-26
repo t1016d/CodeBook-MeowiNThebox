4be2b1149a16b0004da4c5f940abb9bde89c521c
ppetter1025 <peter58972@gmail.com>
2018-09-26 15:43:38 +0800

    delete the useless lines in my code

--- datastructure/DJS.cpp
+++ datastructure/DJS.cpp
@@ -1,29 +1,9 @@
-#include <bits/stdc++.h>
-#define PB push_back
-#define MP make_pair
-#define F first
-#define S second
-#define SZ(x) ((int)(x).size())
-#define ALL(x) (x).begin(),(x).end()
-#ifdef _DEBUG_
-	#define debug(...) printf(__VA_ARGS__)
-#else
-	#define debug(...) (void)0
-#endif
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> PII;
-typedef vector<int> VI;
-
-const int MAXN = 300000 + 10;
-
+const int MAXN = ;
 bool q[MAXN];
-
 struct DisJointSet {
 	int p[MAXN], sz[MAXN], gps;
 	vector<pair<int*, int> > h;
 	VI sf;
-
 	void init(int n) {
 		for(int i=1; i<=n; i++) {
 			p[i] = i;
@@ -31,16 +11,13 @@ struct DisJointSet {
 		}
 		gps = n;
 	}
-
 	void assign(int *k, int v) {
 		h.PB(MP(k, *k));
 		*k = v;
 	}
-
 	void save() {
 		sf.PB(SZ(h));
 	}
-
 	void load() {
 		int last = sf.back();	sf.pop_back();
 		while(SZ(h) != last) {
@@ -48,11 +25,9 @@ struct DisJointSet {
 			*x.F = x.S;
 		}
 	}
-
 	int find(int x) {
 		return x==p[x] ? x : find(p[x]);
 	}
-
 	void uni(int x, int y) {
 		x = find(x), y = find(y);
 		if(x == y)	return ;
@@ -62,11 +37,9 @@ struct DisJointSet {
 		assign(&gps, gps-1);
 	}
 } djs;
-
 struct Seg {
 	vector<PII> es;
 	Seg *tl, *tr;
-
 	Seg() {}
 	Seg(int l, int r) {
 		if(l == r)	tl = tr = NULL;
@@ -76,7 +49,7 @@ struct Seg {
 			tr = new Seg(m+1, r);
 		}
 	}
-
+	// add an edge e from time a to time b
 	void add(int a, int b, PII e, int l, int r) {
 		if(a <= l && r <= b)	es.PB(e);
 		else if(b < l || r < a)	return ;
@@ -86,62 +59,17 @@ struct Seg {
 			tr->add(a, b, e, m+1, r);
 		}
 	}
-
 	void solve(int l, int r) {
 		djs.save();
 		for(auto p : es)	djs.uni(p.F, p.S);
-
 		if(l == r) {
-			if(q[l])	printf("%d\n", djs.gps);
+			if(q[l]);	// answer the query here
 		}
 		else {
 			int m = (l+r) / 2;
 			tl->solve(l, m);
 			tr->solve(m+1, r);
 		}
-
 		djs.load();
 	}
 };
-
-map<PII, int> prv;
-
-int main() {
-	freopen("connect.in", "r", stdin);
-	freopen("connect.out", "w", stdout);
-
-	int n, k;
-	scanf("%d%d\n", &n, &k);
-	if(!k)	return 0;
-
-	Seg *seg = new Seg(1, k);
-	djs.init(n);
-	for(int i=1; i<=k; i++) {
-		char op = getchar();
-		if(op == '?') {
-			q[i] = true;
-			op = getchar();
-		}
-		else {
-			int u, v;
-			scanf("%d%d\n", &u, &v);
-			if(u > v)	swap(u, v);
-			PII eg = MP(u, v);
-			int p = prv[eg];
-			if(p) {
-				seg->add(p, i, eg, 1, k);
-				prv[eg] = 0;
-			}
-			else	prv[eg] = i;
-		}
-	}
-	for(auto p : prv) {
-		if(p.S) {
-			seg->add(p.S, k, p.F, 1, k);
-		}
-	}
-	
-	seg->solve(1, k);
-    
-    return 0;
-}
--- datastructure/ShuLian.cpp
+++ datastructure/ShuLian.cpp
@@ -1,27 +1,8 @@
-#include <bits/stdc++.h>
-#define PB push_back
-#define MP make_pair
-#define F first
-#define S second
-#define SZ(x) ((int)(x).size())
-#define ALL(x) (x).begin(),(x).end()
-#ifdef _DEBUG_
-	#define debug(...) printf(__VA_ARGS__)
-#else
-	#define debug(...) (void)0
-#endif
-using namespace std;
-typedef long long ll;
-typedef pair<int,int> PII;
-typedef vector<int> VI;
-
 const int MAXN = 10000 + 10;
-
 vector<PII> e[MAXN];
 int val[MAXN];
 int sz[MAXN], max_son[MAXN], p[MAXN], dep[MAXN];
 int link[MAXN], link_top[MAXN], cnt;
-
 void find_max_son(int u) {
 	sz[u] = 1;
 	max_son[u] = -1;
@@ -38,7 +19,6 @@ void find_max_son(int u) {
 		sz[u] += sz[v];
 	}
 }
-
 void build_link(int u, int top) {
 	link[u] = ++cnt;
 	link_top[u] = top;
@@ -47,11 +27,9 @@ void build_link(int u, int top) {
 		PII tmp = e[u][i];
 		int v = tmp.F;
 		if(v==p[u] || v==max_son[u])	continue;
-
 		build_link(v, v);
 	}
 }
-
 int query(int a, int b) {
 	int res = -1;
 	int ta = link_top[a], tb = link_top[b];
@@ -60,16 +38,13 @@ int query(int a, int b) {
 			swap(a, b);
 			swap(ta, tb);
 		}
-
 		res = max(res, seg->qry(link[ta], link[a], 1, cnt));
 		ta = link_top[a=p[ta]];
 	}
-
 	if(a != b) {
 		if(dep[a] > dep[b])	swap(a, b);
 		a = max_son[a];
 		res = max(res, seg->qry(link[a], link[b], 1, cnt));
 	}
-
 	return res;
 }
