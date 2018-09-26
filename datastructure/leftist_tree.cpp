4cc4249d538dc6e0fc70cf4cfbe080a9c2ac0618
Lin, Ting-Wei <t1016d@gmail.com>
2018-09-26 21:22:07 +0800

    Remove unnecessary codes and change style (again)

--- datastructure/leftist_tree.cpp
+++ datastructure/leftist_tree.cpp
@@ -48,60 +48,9 @@ void clear(Left* &p) {
 }
 
 int main() {
-	int T,n,x,o,size;
-	bool bst,bqu,bpq;
-	scanf("%d",&T);
-	while(T--) {
-		bst=bqu=bpq=1;
-		stack<int> st;
-		queue<int> qu;
+	while (T--) {
 		clear(root);
-		size=0;
-		scanf("%d",&n);
-		while(n--) {
-			scanf("%d%d",&o,&x);
-			if(o==1)
-				st.push(x),qu.push(x),push(x),size++;
-			else if(o==2) {
-				size--;
-				if(size<0)
-					bst=bqu=bpq=0;
-				if(bst) {
-					if(st.top()!=x)
-						bst=0;
-					st.pop();
-				}
-				if(bqu) {
-					if(qu.front()!=x)
-						bqu=0;
-					qu.pop();
-				}
-				if(bpq) {
-				//	printf("(%d)\n",top());
-					if(top()!=x)
-						bpq=0;
-					pop();
-				}
-			}
-		}
-		int count=0;
-		if(bst)
-			count++;
-		if(bqu)
-			count++;
-		if(bpq)
-			count++;
-	
-		if(count>1)
-			puts("not sure");
-		else if(count==0)
-			puts("impossible");
-		else if(bst)
-			puts("stack");
-		else if(bqu)
-			puts("queue");
-		else if(bpq)
-			puts("priority queue");
+		/** do something **/
 	}
 	return 0;
 }
